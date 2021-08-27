52360a71c33663fd7222810bdb4e9ff3e31b563f
joonlim9 <joonlim@umich.edu>
2021-08-26 22:50:43 -0400

    close matches

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 def print_def(word):
     english_dictionary = json.load(open("english_dictionary.json"))
@@ -11,6 +12,12 @@ def print_def(word):
         return english_dictionary[word.lower()]
     elif word.upper() in english_dictionary: 
         return english_dictionary[word.upper()]
+    elif len(get_close_matches(word, english_dictionary.keys())) > 0:
+        closest = input("Did you mean %s? Enter Y/N (yes/no)" % get_close_matches(word, english_dictionary.keys())[0])
+        if closest == "Y":
+            return english_dictionary[get_close_matches(word, english_dictionary.keys())[0]]
+        elif closest == "N":
+            return "The word you're looking for does not exist"
     else:
         return "The word you're looking for does not exist"
 
